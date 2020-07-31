d595a863cfffd23e0073dbbb697a9de5f901c313
Marc <marc.burchart@fernuni-hagen.de>
2020-07-31 20:03:00 +0200

    added the main functions

--- index.php
+++ index.php
@@ -4,31 +4,45 @@
     global $USER, $PAGE, $DB;
     $PAGE->set_context($context);  
     $PAGE->set_url($CFG->wwwroot.'/local/short/index.php');   
+    $url = new moodle_url('/');
     try{        
         require_login();
         if(!isset($USER->id)) throw new Exception("User id not found.");
         if(!isset($_GET['c']) && !isset($_GET['s'])) throw new Exception("No shortener or course specified.");
         if(isset($_GET['c'])){        
-            $course = $DB->get_records_sql('SELECT c.course_id, c.course_start, c.path FROM '.$CFG->prefix.'cassign_courses AS c LEFT JOIN 
-            '.$CFG->prefix.'cassign_shorts AS s ON s.id = c.short_id WHERE s.short = ?', array($_GET['c']));
-            if(!is_array($course) || count($course) <= 0) throw new Exception("No course found.");
-            $mc = enrol_get_my_courses();        
-
-            var_dump($course);
-            /*$course = $_GET['c'];
-            $uid = $USER->id;     
-            
-            $courses = Array();
-            foreach($mc as $course){
-                $courses[] = $course->id;
-            }  
-            var_dump($courses);*/
-        } else {
-            $short = $_GET['s'];
+            $course = $DB->get_records_sql('SELECT c.course_id, c.path FROM '.$CFG->prefix.'cassign_courses AS c LEFT JOIN 
+            '.$CFG->prefix.'cassign_shorts AS s ON s.id = c.short_id WHERE s.short = ?', array(strtolower($_GET['c'])));
+            if(!is_array($course) || count($course) < 1) throw new Exception("No course found.");
+            $mc = enrol_get_my_courses();      
+            if(!is_array($mc) || count($mc) < 1) throw new Exception("No enrolled course found.");
+            $found = null;            
+            foreach($mc as $ec){
+                foreach($course as $sc){                    
+                    if(+$sc->course_id === +$ec->id){
+                        if(!is_null($found)){
+                            if($ec->startdate > $found->startdate){
+                                $found = new stdClass();
+                                $found->startdate = $ec->startdate;
+                                $found->path = $sc->path;
+                            }
+                        } else {
+                            $found = new stdClass();
+                            $found->startdate = $ec->startdate;
+                            $found->path = $sc->path;
+                        }
+                    }
+                }
+            }
+            if(is_null($found)) throw new Exception("No matched course found.");  
+            echo "Redirect to: ".$found->path;         
+            $url = new moodle_url($found->path);           
+        } else {            
+            $link = $DB->get_records_sql('SELECT extern, link FROM '.$CFG->prefix.'cassign_links WHERE id = ? LIMIT 1', array($_GET['s']));
+            var_dump($link);
         }       
-    } catch(Exception $ex){ 
-        var_dump($ex);
-        //$url = new moodle_url('/');
-        //redirect($url);
+    } catch(Exception $ex){
+        echo $ex->getMessage();
     }
+    //var_dump($url);
+    //redirect($url);
 ?>
