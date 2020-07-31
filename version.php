faec490a4d059557eb3c52c5f5a98585368c037f
Marc <marc.burchart@fernuni-hagen.de>
2020-07-31 20:07:07 +0200

    minor changes

--- version.php
+++ version.php
@@ -12,7 +12,7 @@
     defined('MOODLE_INTERNAL') || die();
     
     $plugin->component = 'local_cassign';
-    $plugin->version = 2020060321;
+    $plugin->version = 2020060322;
     $plugin->requires = 2017050500;    
 
 ?>
