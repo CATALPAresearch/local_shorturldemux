9789818bb24c7452355b9037fe0dbf7ad543da1d
nise <niels.seidel@nise81.com>
2024-04-17 18:06:56 +0200

    update

--- lang/de/local_shorturldemux.php
+++ lang/de/local_shorturldemux.php
@@ -3,8 +3,8 @@
 $string['course'] = "Inhalt iherer Kurswahl:";
 $string['course_ID'] = "Kurs ID auswählen";
 $string['course_button'] = "senden";
-$string['table_change'] = " short Ändern";
-$string['table_change2'] = " path Ändern";
+$string['table_change'] = "Kurz-URL ädern";
+$string['table_change2'] = "Pfad ändern";
 $string['task'] = "Speicheroption auswählen";
 $string['next_entry'] = "nächsten Eintrag erzeugen";
 $string['old_entry'] = "alten Eintrag überschreiben";
--- lang/en/local_shorturldemux.php
+++ lang/en/local_shorturldemux.php
@@ -3,8 +3,8 @@
 $string['course'] = "content of their course choice: ";
 $string['course_ID'] = "chose Course ID";
 $string['course_button'] = "send";
-$string['table_change'] = " short change";
-$string['table_change2'] = " path change";
+$string['table_change'] = " change short URL";
+$string['table_change2'] = "change path";
 $string['task'] = "select issue";
 $string['next_entry'] = "create next entry";
 $string['old_entry'] = "overwrite old entry";
