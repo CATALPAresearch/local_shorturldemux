5d66f3b352d7d0cb77048f3fda91e736cb822775
nise <niels.seidel@nise81.com>
2024-09-26 16:08:59 +0200

    small checks and fixes

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
 <?php
+
 /**
  * TODO
  * The URL parameter s and c need to become more self explaining. 
  * For some reason we mixed them up so that c is used for the shortener string, 
  * instead for the course id.
+ * 
+ * TODO: replace 4_GET by soething like: $shorturl_param = optional_param('shorturl', null, PARAM_ALPHANUMEXT);
+ * if (!$shorturl_param) {
+ *    throw new Exception(get_string('nothing_specified', 'local_shorturldemux'));
+ *}
  * 
  * @category Moodle
  * @package  Local_ShortURLdemux
@@ -12,33 +18,33 @@
  * @link     URL shortener, short URL
  */
 
-require_once dirname(__FILE__) . '/../../config.php';   
+require_once dirname(__FILE__) . '/../../config.php';
 
 $context = context_system::instance();
 global $USER, $PAGE, $DB;
-$PAGE->set_context($context);  
-$PAGE->set_url($CFG->wwwroot.'/local/shorturldemux/index.php');
-if (!isset($_GET['c'])) {   
-    $PAGE->set_url($CFG->wwwroot.'/local/shorturldemux/index.php', array('c' => $_GET['c'])); 
+$PAGE->set_context($context);
+$PAGE->set_url($CFG->wwwroot . '/local/shorturldemux/index.php');
+if (!isset($_GET['c'])) {
+    $PAGE->set_url($CFG->wwwroot . '/local/shorturldemux/index.php', array('c' => $_GET['c']));
 }
 
 $url = new moodle_url('/');
 
-// Store the requested URL in a coockie to make the redirect happen after the login was successful
-if(!isset($_SESSION['shorturlredirect']) && isset($_GET['c'])){
+// Store the requested URL in a session to redirect after login
+if (!isset($_SESSION['shorturlredirect']) && isset($_GET['c'])) {
     $_SESSION['shorturlredirect'] = '/local/shorturldemux/index.php?c=' . $_GET['c'];
 }
 
 require_login();
 
 
-try{        
-    
+try {
+
     // redirect to the URL stored in the cockie if the parameterc is not set
-    if(isset($_SESSION['shorturlredirect']) && !isset($_GET['c'])){
+    if (isset($_SESSION['shorturlredirect']) && !isset($_GET['c'])) {
         $url = new moodle_url($_SESSION['shorturlredirect']);
         unset($_SESSION['shorturlredirect']);
-        header('Location: '.$url); 
+        header('Location: ' . $url);
     }
     if (!isset($USER->id)) {
         throw new Exception(get_string('userid_not_found', 'local_shorturldemux'));
@@ -50,27 +56,27 @@ try{
         if (is_string($_GET['c']) == false || strlen($_GET['c']) > 200) {
             exit;
         }
-        
+
         $course = $DB->get_records_sql(
             'SELECT c.course_id, c.path 
-            FROM '.$CFG->prefix.'shorturldemux_courses AS c 
+            FROM ' . $CFG->prefix . 'shorturldemux_courses AS c 
             WHERE c.short = ?',
             array(strtolower($_GET['c']))
-        ); 
+        );
         if (!is_array($course) || count($course) < 1) {
             throw new Exception(
                 get_string('no_course_found', 'local_shorturldemux')
             );
-        } 
-        $mc = enrol_get_my_courses();      
+        }
+        $mc = enrol_get_my_courses();
         if (!is_array($mc) || count($mc) < 1) {
             throw new Exception(
                 get_string('no_course_enrolled', 'local_shorturldemux')
             );
-        } 
-        $found = null;            
+        }
+        $found = null;
         foreach ($mc as $ec) {
-            foreach ($course as $sc) {                    
+            foreach ($course as $sc) {
                 if (+$sc->course_id === +$ec->id) {
                     if (!is_null($found)) {
                         if ($ec->startdate > $found->startdate) {
@@ -89,15 +95,12 @@ try{
         if (is_null($found)) {
             throw new Exception(
                 get_string('no_matching_course', 'local_shorturldemux')
-            );  
+            );
         }
         $url = new moodle_url($found->path);
-        header('Location: '.$url);
-        
+        header('Location: ' . $url);
     }
-} catch(Exception $ex) {
-    echo $ex->getMessage();
+} catch (Exception $ex) {
+    print_error($ex->getMessage());
     exit;
 }
-    
-?>
